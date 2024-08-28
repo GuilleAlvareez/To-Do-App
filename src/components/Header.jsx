0a376a029129e5f7f44a4b50bc365820d976163a
Guille_alvareez <guillealvarezmoreno@gmail.com>
2024-08-28 18:06:08 +0200

    Svg header ahora si se ve

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header({ isOpen, setIsOpen }) {
     return (
     <header className=" fixed top-0 left-0 w-full z-10 bg-white shadow-lg flex flex-row gap-6 pl-5 h-16 items-center">
         <button onClick={toggleButtonSideBar} className="w-6 abs">
-          <img className="w-full h-full" src="/public/sideBarIcon.svg" alt="SideBar"/>
+          <img className="w-full h-full" src="/sideBarIcon.svg" alt="SideBar"/>
         </button>
 
         <span className="text-xl font-bold text-primary">ToDoApp</span>
