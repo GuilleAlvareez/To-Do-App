ada7ccf3eb3f697276f58b24e7d47e1f17ac39a6
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2024-10-17 17:43:15 +0200

    Resumen de tareas arreglado añadiendo un filter para filtrar por categoria para saber cuantas hay de cada una

--- src/components/ResumeTask.jsx
+++ src/components/ResumeTask.jsx
@@ -4,23 +4,10 @@ import { listTaskContext } from "../contexts/setOfTask"
 export function ResumeTask() {
     const { listTasks } = useContext(listTaskContext)
 
-    //ARREGLARS
-    const pendingTask = () => {
-        let cont = 0
-
-        Array.from(listTasks).forEach((task) => {
-            if (!task.completed) {
-                cont++
-            }
-            return cont
-        })
-    }
-
     return (
         <article className="flex flex-col mx-5 rounded border p-4 gap-2 border-gray-300 mb-5">
           <label>Total tasks: {Array.from(listTasks).length}</label>
-          <label>Tasks completed:</label>
-          <label>Pending tasks:</label>
+          <label>Pending tasks:  {Array.from(listTasks).filter((task) => !task.completed).length}</label>
         </article>
     )
 }
